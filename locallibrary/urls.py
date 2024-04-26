dc6a3cf989b2b59c8b578d3d6c1eecd3ff81d462
ledsouza <leandro.souza.159@gmail.com>
2024-04-26 11:05:07 -0300

    fixed url mapping in locallibrary/url.py

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -23,5 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
-    path('catalog', include('catalog.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('catalog/', include('catalog.urls'))
+]
